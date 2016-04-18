e6a9a9b7227a3695a25aa7f03d6237f6af0f0fe8
Dwayne Ford <dwayneford@gmail.com>
2016-04-18 02:55:10 -0500

    First e2e tests

---
+++ tests/end-to-end/firemodel_tests.js
@@ -0,0 +1,38 @@
+'use strict';
+
+describe('Firemodel', function () {
+
+  xit('should modify firebase prototype', function () {});
+  xit('should be available as model attribute of a ref', function () {});
+  xit('should be a function', function () {});
+  xit('should return an object', function () {});
+
+  describe('Class (object)', function () {
+
+    xit('should have a (D)elete property', function () {});
+    xit('(D)elete should be a function', function () {});
+
+    xit('should have an (A)dd property', function () {});
+    xit('(A)dd should be a function', function () {});
+
+    xit('should have an (V)iew property', function () {});
+    xit('(V)iew should be a function', function () {});
+
+    xit('should have an (I)ndex property', function () {});
+    xit('(I)ndex should be a function', function () {});
+
+    xit('should have an (E)dit property', function () {});
+    xit('(E)dit should be a function', function () {});
+
+    xit('should have a (C)reate property', function () {});
+    xit('(C)reate should be a function', function () {});
+
+    xit('should have a (R)ead property', function () {});
+    xit('(R)ead should be a function', function () {});
+
+    xit('should have a (U)pdate property', function () {});
+    xit('(U)pdate should be a function', function () {});
+
+  });
+
+});
